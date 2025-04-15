3e13ebcd73b0b49e1d2ec77288403ec4586cd8ab
awd17 <augdrak17@gmail.com>
2025-04-15 04:47:03 -0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+    <>
+      <nav className="flex items-center justify-between w-full max-w-3xl m-auto">
+        <Link href="/">
+          <h1 className="text-2xl font-geist-mono font-bold">
+            august<span className="text-neutral-500/50">.fyi</span>
+          </h1>
+        </Link>
+        <ul className="flex space-x-4 font-geist-mono font-semibold">
+          <li>
+            <Link href="#about">About</Link>
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li>
+            <Link href="#projects">Projects</Link>
           </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          <li>
+            <Link href="#contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <main className="flex flex-col space-y-12 w-full max-w-3xl m-auto mt-8">
+        <section id="about" className="font-geist-sans space-y-4">
+          <div className="font-departure-mono text-lg">
+            <p className="mb-3">
+              Hey, my name is{" "}
+              <span className="whitespace-nowrap">August Drakton 👋</span>
+            </p>
+            <p className="mb-3">
+              I'm a <span className="whitespace-nowrap">student 👨‍🎓</span> based
+              in Los Angeles,<span className="whitespace-nowrap"> CA 📍</span>
+            </p>
+            <p>
+              {" "}
+              {/* No bottom margin needed on the last paragraph */}
+              Check out what I've{" "}
+              <span className="whitespace-nowrap">developed 👨‍💻</span> and get in
+              touch with <span className="whitespace-nowrap">me! 📬</span>
+            </p>
+          </div>
+        </section>
+        <section id="projects" className="font-geist-sans space-y-4">
+          <h2 className="font-inter font-semibold text-lg"></h2>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
